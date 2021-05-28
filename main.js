4e267f5bb1beb2067a6eaf94562a1aaee8ade550
boa959Cosmo <noah@web5.ch>
2021-05-28 16:32:57 +0200

    lets goooooooooooooooooooo

--- main.js
+++ main.js
@@ -3,30 +3,31 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false,
-    }
+	width: 1600,
+	height: 1200,
+	frame: true,
+	transparent: true,
+	webPreferences: {
+		preload: path.join(__dirname, 'preload.js'),
+		nodeIntegration: true,
+		contextIsolation: false,
+	}
   })
 
   win.loadFile('./renderer/index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+	createWindow()
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
--- renderer/script.js
+++ renderer/script.js
@@ -1,21 +1,39 @@
 const fs = require("fs");
-const { Math } = require("globalthis/implementation");
 
 document.getElementById("files").innerHTML = fs.readdirSync("./")
 
 function test() {
-  console.log("ja");
-    var y = document.getElementById('map')
-    var x = document.createElement('DIV')
-    var textnode = document.createTextNode("Water"); 
-    x.appendChild(textnode)
-    x.setAttribute('id','noahlost')
-    x.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
-    x.style.height = `${Math.random() * 20}px`
-    y.appendChild(x)
-    window.scrollTo(0,document.body.scrollHeight);
+	console.log("ja");
+	let y = document.getElementById('map')
+	let x = document.createElement('DIV')
+	x.setAttribute('id','noahlost')
+	x.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+	x.style.height = `${Math.random() * 20}px`
+	y.appendChild(x)
+	window.scrollTo(0,document.body.scrollHeight)
 
-    setTimeout(() => {
-      test()
-    }, Math.random() * 50);
+	setTimeout(() => {
+		test()
+	}, Math.random() * 50);
+}
+
+function generateMap() {
+    const mapScale = 4
+    const mapBluePrint = []
+    for(i = 0; mapScale >= i; ++i) { //generate Map
+        for (j = 0; mapScale >= j; ++j) {
+            mapBluePrint.push({x : j, y : i})
+        }
+    }
+    const map = document.getElementById('map')
+
+    for(i = 0; mapBluePrint.length >= i; ++i) { // Apply Map
+        let grid = document.createElement('div')
+        grid.setAttribute('class', 'grid')
+        grid.setAttribute('id', `${i}`) 
+        grid.style["grid-column"] = mapBluePrint[(i+1)].x
+        map.appendChild(grid)
+        console.log(mapBluePrint[i])
+
+    }
 }
